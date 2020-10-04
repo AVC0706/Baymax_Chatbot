6c71ccf6a8fd70e79817dd04da350c9875d6df51
AVC0706 <atharvvijay4@gmail.com>
2020-10-04 18:26:17 +0530

    Login page done

--- Web/client/src/routes.js
+++ Web/client/src/routes.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { Route, Router, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import login from './pages/login/login.page';
-import HealthForm from './pages/login/userHealthForm';
+import HealthForm from './pages/healthForm/userHealthForm';
 
 class Routes extends React.Component {
   render() {
     return (
       <Switch>
         {/* <Route path="/" exact component={HomePage} />*/}
-        <Route exact path='/' component={login} />
+        <Route exact path='/login' component={login} />
         <Route exact path='/healthform' component={HealthForm}></Route>
       </Switch>
     );
